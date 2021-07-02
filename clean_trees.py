8bdebd592cb925179257c9619f28e69e99754b97
AmauryvanKeste <you@domain.com>
2021-07-02 16:39:24 +0200

    last changes

--- clean_trees.py
+++ clean_trees.py
@@ -1,22 +1,18 @@
 ﻿import pandas as pd
-import numpy as np
 
+# downloaded original file: 250 MB
+# work on small datasets by changing the iloc slicing
 directory = r"C:\Users\Amaury\Documents\Data_Science_BECODE\Projects\Python\GNT-Arai-2.31\content\4.machine_learning\0.data_preprocessing\Nycity-trees\2015_Street_Tree_Census_-_Tree_Data.csv"
 df_data_100000_ori = pd.read_csv(directory, sep=',').iloc[0:100000,:]
 df_data_100000 = df_data_100000_ori.copy()
-print(df_data_100000.shape)
-#print(df_data_100000)
-#print(df_data_100000.columns)
-# print(df_data_100000.shape)
-print(df_data_100000.info)
-# print(df_data_100000.dtypes)
-#df_data_100000.head()
+
 
 def remove_empty_rows(df):
     return df.dropna(how='all')
 
+# drop duplicates in the tree_id column
 def drop_duplicates(column):
-    df_data_100000.drop_duplicates(subset=column, keep=False)
+    return df_data_100000.drop_duplicates(subset=column, keep=False)
 
 
 def fill_undefined(column):
@@ -48,31 +44,21 @@ def change_no_yes_to_false_true(columns_list):
         df_data_100000[column] = df_data_100000[column].replace(to_replace="No", value=False)
         df_data_100000[column] = df_data_100000[column].replace(to_replace="Yes", value=True)
 
+
 def change_to_category(column):
     df_data_100000[column] = df_data_100000[column].astype("category")
-# print(df_data_100000)
-# print(df_data_100000.isnull().any())
-# print(df_data_100000.isna().sum())
-
-# columns_to_float =
-# columns to
-
-# def drop_duplicates(column, replace_nan_value):
-#     self.df[column] = self.df[column].fillna(replace_nan_value)
-#
-# def apply_fillna_to_column(self, column, replace_nan_value):
-#     self.df[column] = self.df[column].fillna(replace_nan_value)
-#
-# def count_nans_in_df(self):
-#     return self.df.isna().sum()
 
 
-# first drop duplicates in the tree_id column
+def write_to_csv(file_path):
+    df_data_100000.to_csv(file_path)
 
+# drop the redundant columns
 df_data_100000 = df_data_100000.drop(["state", "borocode", "nta", "problems"], axis = 1)
 
+# remove empty rows, if any
 remove_empty_rows(df_data_100000)
 
+# drop duplicates, if any
 drop_duplicates("tree_id")
 
 # add undefined in the object columns
@@ -110,9 +96,8 @@ change_to_category(columns_to_category)
 # check types
 print(df_data_100000.dtypes)
 
+# lower case everythin for consistency
 columns_to_be_lowercased = ["curb_loc", "status", "health", "spc_latin", "spc_common", "nta_name", "steward", "guards", "sidewalk", "user_type", "address", "zip_city", "borough"]
-#lowercase(columns_to_be_lowercased)
-
 df_data_100000[columns_to_be_lowercased] = df_data_100000[columns_to_be_lowercased].apply(lambda value: value.astype(str).str.lower())
 
 # checks missing data:
@@ -124,7 +109,4 @@ print(df_data_100000.shape)
 # print(df_data_100000.dtypes)
 
 # make clean csv file
-def write_to_csv(file_path):
-    df_data_100000.to_csv(file_path)
-
 write_to_csv("clean_trees.csv")
